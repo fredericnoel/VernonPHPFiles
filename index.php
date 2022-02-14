c627b504c190c1130ed41723463b53c080b057ee
Frédéric NOËL <contact@fredericnoel.com>
2022-02-14 16:31:29 +0100

    Test méthode statique write() de la classe Log

--- index.php
+++ index.php
@@ -8,3 +8,5 @@ file_put_contents('./files/toto.txt', 'Ecriture dans fichier');
 $contenu = file_get_contents('./files/toto.txt');
 $contenu .= "\nLa suite du fichier";
 file_put_contents('./files/toto.txt', $contenu, FILE_APPEND);
+
+Log::write('Erreur');
