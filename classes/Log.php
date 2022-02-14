07b73a02a918b2169ade817a21c3b0c46995b6ee
Frédéric NOËL <contact@fredericnoel.com>
2022-02-14 16:43:25 +0100

    Remplacement de file_put_contents() par fwrite()

--- classes/Log.php
+++ classes/Log.php
@@ -2,8 +2,12 @@
 
 class Log
 {
+    public static $path = "./logs/erreur.txt";
+
     public static function write($error)
     {
-        file_put_contents('./logs/erreur.txt', $error);
+        $fichier = fopen(self::$path, 'c+b');
+        fwrite($fichier, "\n$error");
+        fclose($fichier);
     }
 }
