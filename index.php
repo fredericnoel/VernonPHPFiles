bd2f98f95d3bc3e8d6e97d5cf69f20db26180b56
Frédéric NOËL <contact@fredericnoel.com>
2022-02-14 12:16:03 +0100

    Fonction readfile()
    Affiche le fichier tel que (sans variable, sans tableau)

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
 <?php
 
-$fichier = file('./files/corbeau.txt');
-
-$fable = "<blockquote>";
-
-for($i = 0 ; $i < count($fichier) ; $i++) {
-    $fable .= $fichier[$i];
-    $fable .= "<br />";
-}
-
-$fable .= "</blockquote>";
-
-echo $fable;
-
-echo "<hr />";
-
-// Affiche le fichier avec mise en forme d'origine (retours chariot remplacés par <br />)
-echo nl2br(file_get_contents('./files/corbeau.txt'));
+readfile('./files/corbeau.txt');
