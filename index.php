bef5dc11a41225ad57434241dd1218c5dd5f5d1a
Frédéric NOËL <contact@fredericnoel.com>
2022-02-14 12:13:14 +0100

    Fonction file()
    La fonction renvoie un tableau indicé.
    Fonction nl2br()

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
 
-$fichier1 = file_get_contents('./files/corbeau.txt');
-echo "<pre>";
-echo $fichier1;
-echo "</pre>";
+$fichier = file('./files/corbeau.txt');
+
+$fable = "<blockquote>";
+
+for($i = 0 ; $i < count($fichier) ; $i++) {
+    $fable .= $fichier[$i];
+    $fable .= "<br />";
+}
+
+$fable .= "</blockquote>";
+
+echo $fable;
+
+echo "<hr />";
+
+// Affiche le fichier avec mise en forme d'origine (retours chariot remplacés par <br />)
+echo nl2br(file_get_contents('./files/corbeau.txt'));
